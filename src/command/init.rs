d6f2e92c1de79ff7e1b238b94a0c8be2058f8b59
Jascha030 <contact@jaschavanaalst.nl>
2022-04-18 13:12:13 +0200

    🚧 create init.rs and open.rs for both commands.

---
+++ src/command/init.rs
@@ -0,0 +1,14 @@
+mod init;
+
+use std::io::Result;
+
+impl Run for init {
+    fn run(&self) -> Result<()> {
+        //let mut buffer String::new();
+        //let mut stdin = io::stdin();
+        //stdin.read_line(&mut buffer)?;
+        
+        Ok(())
+    }
+}
+
