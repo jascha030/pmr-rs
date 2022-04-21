2c805e08d5e13dc99fda5c145be789379a7d48f3
Jascha030 <contact@jaschavanaalst.nl>
2022-04-21 18:58:42 +0200

    ✨ Add function to parse to toml.

---
+++ src/resource/write.rs
@@ -0,0 +1,20 @@
+use std::io::{Result, Write};
+use std::fs::OpenOptions;
+
+use super::Resource;
+
+pub fn write_toml(resources: &[Resource]) -> Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(".pm.toml")
+        .unwrap();
+
+    for res in resources {
+        let toml_string = toml::to_string(&res).unwrap();
+        file.write_all(&toml_string.as_bytes())
+            .expect("Unable to write to file");
+    }
+
+    Ok(())
+}
