e1bf450fadfd76fc9e8501d9e1d8c3592192379e
Jascha030 <contact@jaschavanaalst.nl>
2022-04-21 19:24:19 +0200

    💩 Testing functionality in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ mod command;
 mod resource;
 
 use std::io::Result;
+use std::panic;
+
+use self::resource::Resource;
+use self::resource::write::write_toml;
 
 fn main() -> Result<()> {
+    let arr = [Resource {
+        name: "ewa".to_string(),
+        url: "https://google.com".to_string(),
+    }];
+
+    return match write_toml(&arr) {
+        Ok(_) => Ok(()),
+        Err(e) => panic!("No gucci, no bueno {:?}", e)
+    }
+
     //return match Command::parse().run() {
     //    Ok(_) => Ok(()),
-    //    Err(e) => Err(),
+    //    Err(e) => panic!("Ewa fakka man niffo {:?}", e),
     //};
-
-    Ok(())
 }
