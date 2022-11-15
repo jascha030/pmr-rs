0304e6ebb13615b7e2d3aeb97871cdcd3b428965
Jascha030 <contact@jaschavanaalst.nl>
2022-11-15 21:13:35 +0100

    ♻️ Refactor write_toml function to take Config struct instead of plain array of Resources.

--- src/resource/write.rs
+++ src/resource/write.rs
@@ -1,19 +1,26 @@
 use std::fs::OpenOptions;
 use std::io::{Result, Write};
 
-use super::Resource;
+use super::{Config, Resource};
 
-pub fn write_toml(resources: &[Resource]) -> Result<()> {
+pub fn write_toml(config: &Config) -> Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .open(".pm.toml")
         .unwrap();
 
-    for res in resources {
-        let toml_string = toml::to_string(&res).unwrap();
-        file.write_all(&toml_string.as_bytes())
-            .expect("Unable to write to file");
+    let arr = [&config.tasks, &config.time, &config.git];
+
+    for res in arr {
+        match res {
+            Some(r) => {
+                let toml_string = toml::to_string(r).unwrap();
+                file.write_all(&toml_string.as_bytes())
+                    .expect("Unable to write to file");
+            }
+            None => (),
+        }
     }
 
     Ok(())
