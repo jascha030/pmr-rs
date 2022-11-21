f2ce7dcec12c70841bba033c82249c98b6d27b60
Jascha030 <contact@jaschavanaalst.nl>
2022-11-21 13:34:10 +0100

    🚧 Open logic.

--- src/command/open.rs
+++ src/command/open.rs
@@ -1,9 +1,30 @@
-use std::io::Result;
+use std::fs::File;
+use std::io::{Result, BufReader, Read};
+use std::path::Path;
+
+use crate::config::Config;
 
 use super::{Open, Run};
 
 impl Run for Open {
     fn run(&self) -> Result<()> {
+        let mut valid: bool;
+
+        valid = Path::new(".pm.toml").exists();
+
+        if valid == true {
+            let mut file = File::open(".pm.toml")?;
+            let mut buf_reader = BufReader::new(file);
+            let mut contents = String::new();
+            buf_reader.read_to_string(&mut contents)?;
+
+            let config: Config = toml::from_str(&contents).unwrap();
+
+            println!("{:?}", config);
+        } else {
+            println!("No `.pm.toml` file found in current directory.");
+        }
+
         Ok(())
     }
 }
